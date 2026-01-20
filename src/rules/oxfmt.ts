51af2d6b60d3cf0f0ee80cc4a15b4e9834b93dda
ntnyq <ntnyq13@gmail.com>
2026-01-20 11:18:42 +0800

    fix: avoid using json schema `definitions`

--- src/rules/oxfmt.ts
+++ src/rules/oxfmt.ts
@@ -31,48 +31,39 @@ export const oxfmt: Rule.RuleModule = {
       {
         additionalProperties: false,
         type: 'object',
-        definitions: {
-          FormatConfig: {
-            additionalProperties: false,
-            type: 'object',
-            properties: {
-              ...sharedSchema.properties,
-            },
-          },
-          OxfmtOverrideConfig: {
-            additionalProperties: false,
-            required: ['files'],
-            type: 'object',
-            properties: {
-              excludeFiles: {
-                description: `Glob patterns to exclude from this override.`,
-                type: 'array',
-                items: {
-                  type: 'string',
-                },
-              },
-              files: {
-                description: `Glob patterns to match files for this override.\nAll patterns are relative to the Oxfmt configuration file.`,
-                type: 'array',
-                items: {
-                  type: 'string',
-                },
-              },
-              options: {
-                $ref: '#/definitions/FormatConfig',
-                description: `Format options to apply for matched files.`,
-                type: 'object',
-              },
-            },
-          },
-        },
         properties: {
           ...sharedSchema.properties,
           overrides: {
             description: `File-specific overrides.\nWhen a file matches multiple overrides, the later override takes precedence (array order matters).\n\n- (Default: [])`,
             type: 'array',
             items: {
-              $ref: '#/definitions/OxfmtOverrideConfig',
+              additionalProperties: false,
+              required: ['files'],
+              type: 'object',
+              properties: {
+                excludeFiles: {
+                  description: `Glob patterns to exclude from this override.`,
+                  type: 'array',
+                  items: {
+                    type: 'string',
+                  },
+                },
+                files: {
+                  description: `Glob patterns to match files for this override.\nAll patterns are relative to the Oxfmt configuration file.`,
+                  type: 'array',
+                  items: {
+                    type: 'string',
+                  },
+                },
+                options: {
+                  additionalProperties: false,
+                  description: `Format options to apply for matched files.`,
+                  type: 'object',
+                  properties: {
+                    ...sharedSchema.properties,
+                  },
+                },
+              },
             },
           },
         },
